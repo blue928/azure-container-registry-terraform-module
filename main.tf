c428ffe7ed463e38b6ee41ffeda3d5dec3dc8c34
Blue Presley <bluepresley@gastonia.com>
2022-03-08 08:43:55 -0500

    initial commit

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE AN AZURE CONTAINER REGISTRY FOR IMAGES
+# ---------------------------------------------------------------------------------------------------------------------
+# The Azure Container Registry
+# https://docs.microsoft.com/en-us/cli/azure/acr?view=azure-cli-latest
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/container_registry
 
+resource "azurerm_container_registry" "k8s_acr" {
+  name                = var.acr_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.acr_sku
+  admin_enabled       = var.acr_admin_enabled
+
+  tags = {
+    #environment = var.environment
+  }
+}
+#}
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,4 @@
-
+output "container_registry_id" {
+    description = "The ID of the Container Registry."
+    value = azurerm_container_registry.k8s_acr.id
+}
