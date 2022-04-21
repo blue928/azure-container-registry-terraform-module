c25882f4eac94c07a9ffd3a8277567d2f3facecb
Blue Presley <bluepresley@gastonia.com>
2022-04-21 11:41:54 -0400

    update

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,14 @@ output "container_registry_id" {
   description = "The ID of the Container Registry."
   value       = azurerm_container_registry.k8s_acr.id
 }
+
+output "admin_username" {
+  description = "The admin username for the Container Registry."
+  value       = azurerm_container_registry.k8s_acr.admin_username
+}
+
+output "admin_password" {
+  description = "The admin password for the Container Registry."
+  value       = azurerm_container_registry.k8s_acr.admin_password
+  
+}
